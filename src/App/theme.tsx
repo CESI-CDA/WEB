fe4a06d714ce26477575a9d33337c194412cedc0
JulienForesier <forestierjulien3@gmail.com>
2024-04-08 10:45:11 +0200

    feat(theme): add a theme file

---
+++ src/App/theme.tsx
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme(
+  {
+    palette: {
+      primary: {
+        main: "#ff1744",
+      },
+      secondary: {
+        main: "#f50057",
+      },
+    },
+  },
+  {
+    typography: {
+      fontFamily: "Roboto",
+    },
+  }
+);
