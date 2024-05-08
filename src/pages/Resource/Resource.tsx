7dbd6c6243863854afc353269862fc533594d7f0
LnVsr59 <helene.vasseur1@viacesi.fr>
2024-05-08 17:50:01 +0200

    CCC-60 : mise à jour css pour responsive

--- src/pages/Resource/Resource.tsx
+++ src/pages/Resource/Resource.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from "./Resource.module.scss";
-import HeaderResource from './Component/HeaderResource/HeaderResource';
+import HeaderResource from './components/HeaderResource/HeaderResource';
 
 const Resource: React.FC = () => {
     return (
