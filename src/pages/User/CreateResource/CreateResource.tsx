232db6af3f18b77cbb5922bee1f80689725493d5
LnVsr59 <helene.vasseur1@viacesi.fr>
2024-06-02 12:23:16 +0200

    CCC-63 : amélioration visuel du formulaire de création de ressource

--- src/pages/User/CreateResource/CreateResource.tsx
+++ src/pages/User/CreateResource/CreateResource.tsx
@@ -1,55 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './CreateResource.module.scss';
 import HeaderCreateResource from './components/HeaderCreateResource/HeaderCreateResource';
 
-
 const CreateResource: React.FC = () => {
+    const [imagePreviewUrl, setImagePreviewUrl] = useState<string | null>(null);
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImagePreviewUrl(reader.result as string);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
     return (
         <div className={`card flex-fill d-flex flex-column p-30 mb-30 ${styles.contentCard}`}>
             <div className={styles.header}>
                 <HeaderCreateResource title="Créer une nouvelle ressource" />
             </div>
             <div className={styles.formContainer}>
-                <input type="file" accept="image/*" className={styles.input} />
+                <input type="file" accept="image/*" className={styles.input} onChange={handleImageChange} />
 
-                <input
-                    type="text"
-                    placeholder="Title"
-                    className={styles.input}
-                />
+                {imagePreviewUrl && <img src={imagePreviewUrl} alt="Preview" className={styles.imagePreview} />}
 
-                <textarea
-                    placeholder="Content"
-                    className={styles.input}
-                />
+                <input type="text" placeholder="Title" className={styles.input} />
+
+                <textarea placeholder="Content" className={styles.input} />
 
                 <select className={styles.input}>
                     <option value="">Type de ressource</option>
-                    {/* Remplacer les options suivantes par des données dynamiques */}
-                    <option value="1">Type 1</option>
-                    <option value="2">Type 2</option>
+                    <option value="2">News</option>
+                    <option value="3">Exercice / Atelier</option>
+                    <option value="3">Documentaire</option>
                 </select>
 
-
-
+                <select className={styles.input}>
+                    <option value="">Catégorie</option>
+                    <option value="1">Communication</option>
+                    <option value="2">Culture</option>
+                    <option value="3">Sport</option>
+                    <option value="4">Technologie</option>
+                    <option value="5">Finance</option>
+                    <option value="6">Education</option>
+                    <option value="7">Santé</option>
+                </select>
                 <select className={styles.input}>
                     <option value="">Visibilité</option>
-                    {/* Remplacer les options suivantes par des données dynamiques */}
-                    <option value="1">Visibilité 1</option>
-                    <option value="2">Visibilité 2</option>
+                    <option value="1">Public</option>
+                    <option value="2">Privée</option>
                 </select>
 
                 <select className={styles.input}>
-                    <option value="">Pour qui ?</option>
-                    {/* Remplacer les options suivantes par des données dynamiques */}
-                    <option value="1">Relation 1</option>
-                    <option value="2">Relation 2</option>
+                    <option value="">Relation</option>
+                    <option value="1">Famille</option>
+                    <option value="2">Ami</option>
                 </select>
 
-                <button className={styles.button}>Publier</button>
+                <div className="buttonContainer">
+                    <button className={styles.button}>Publier</button>
+                </div>
             </div>
         </div>
-
     );
 };
 
