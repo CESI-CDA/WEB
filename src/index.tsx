0f304d11ae9a59ae5b2b907fa987936078a4679d
JulienForesier <forestierjulien3@gmail.com>
2024-06-05 09:54:28 +0200

    feat: add persistance auth

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,5 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </StrictMode>
+  <RouterProvider router={router}></RouterProvider>
 );
