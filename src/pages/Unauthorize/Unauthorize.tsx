3b76ce018a6d8c4142f34ccd9f81aef5b3d50023
JulienForesier <forestierjulien3@gmail.com>
2024-06-05 16:33:54 +0200

    feat: unauthorizes page

--- src/pages/Unauthorize/Unauthorize.tsx
+++ src/pages/Unauthorize/Unauthorize.tsx
@@ -1,7 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 export const Unauthorize = () => {
   return (
-    <div>
-      <h1>Unauthorized</h1>
+    <div className="d-flex flex-column justify-content-center flex-fill align-items-center">
+      <h1 className="mb-20">
+        Vous n'étes pas autoriser a acceder a cette page
+      </h1>
+      <button className="btn btn-primary">
+        <NavLink to="/" style={{ color: "white" }}>
+          Retour a l'accueil
+        </NavLink>
+      </button>
     </div>
   );
 };
