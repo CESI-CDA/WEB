312f1e05238a77483d306d55b1c16ffaadc5707d
JulienForesier <forestierjulien3@gmail.com>
2024-06-06 19:44:58 +0200

    fix: loader ressource

--- src/pages/Resource/Resource.tsx
+++ src/pages/Resource/Resource.tsx
@@ -7,7 +7,7 @@ import { set } from "react-hook-form";
 import { createComment } from "../../apis/comment";
 import { AuthContext } from "../../context";
 import Modal from "../../components/Modal/Modal";
-
+import Loader from "../../components/Loader/Loader";
 
 const Resource: React.FC = () => {
   const [ressource, setRessources] = useState<any>(null);
@@ -18,15 +18,18 @@ const Resource: React.FC = () => {
   const inputRef = useRef<HTMLInputElement>();
   const [commentText, setCommentText] = useState<string>("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-
+  const [loading, setLoading] = useState<boolean>(null);
 
   useEffect(() => {
     const fetchressource = async () => {
       try {
+        setLoading(true);
         const data = await getRessource(id);
         setRessources(data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching resource data:", error);
+        setLoading(false);
       }
     };
     fetchressource();
@@ -42,7 +45,6 @@ const Resource: React.FC = () => {
       createComment(id_res, id_user, token, text);
       setComment(false);
       setIsModalOpen(true);
-
     } catch (e) {
       console.error("Error creating comment:", e);
     }
@@ -54,103 +56,121 @@ const Resource: React.FC = () => {
 
   return (
     <div className={`flex-fill container d-flex flex-column p-20`}>
-      <div
-        className={`card flex-fill d-flex flex-column p-20 mb-20 ${styles.contentCard}`}
-      >
-        <div className={styles.header}>
-          {ressource && <HeaderResource resourceId={ressource.item.id} />}
-        </div>
-        {ressource && (
-          <>
-            <img
-              src={ressource.item.url_res}
-              alt="Resource"
-              className={styles.imageContainer}
-            />
-            <div className={styles.characteristicProp}>
-              <p className={styles.category}>
-                Catégorie :{" "}
-                {ressource.item.get_lien_ressource_categorie
-                  .map(
-                    (lien: { get_categorie: { intitule_cat: string } }) =>
-                      lien.get_categorie.intitule_cat
-                  )
-                  .join(" / ")}
-              </p>
-              <p className={styles.typeOfRelation}>
-                Types de relation :{" "}
-                {ressource?.item.get_lien_ressource_relation
-                  .map(
-                    (lien: {
-                      get_relation_ressource: { intitule_rel: string };
-                    }) => lien.get_relation_ressource.intitule_rel
-                  )
-                  .join(" / ")}
-              </p>
-              <p className={styles.typeOfResource}>
-                Type de ressource :{" "}
-                {ressource.item.get_type_ressource.intitule_type_res}
-              </p>
-            </div>
-            <div className={styles.content}>
-              <h1 className={styles.title}>{ressource.item.titre_res}</h1>
-              <p className={styles.resourceContent}>
-                {ressource.item.contenu_res}
-              </p>
-            </div>
-          </>
-        )}
-        <div className={styles.headerComment}>
-          <h3 className={styles.comment}>Commentaires</h3>
-          <button
-            className={styles.icon}
-            onClick={() => user && setComment(true)}
+      {loading === true ? (
+        <Loader />
+      ) : (
+        <>
+          <div
+            className={`card flex-fill d-flex flex-column p-20 mb-20 ${styles.contentCard}`}
           >
-            +
-          </button>
-        </div>
-        {comment && (
-          <>
-            <input
-              ref={inputRef}
-              type="text"
-              placeholder="Vous pouvez écrire votre commentaire"
-              value={commentText}
-              onChange={(e) => setCommentText(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  postComment(
-                    ressource.item.id,
-                    user.id,
-                    token,
-                    inputRef.current.value
-                  );
-                }
-              }}
-            ></input>
-            <div className={styles.publish}>
-            <button className="btn btn-primary" onClick={() => postComment(ressource.item.id, user.id, token, commentText)}>
-              Soumettre le commentaire
-            </button>
+            <div className={styles.header}>
+              {ressource && <HeaderResource resourceId={ressource.item.id} />}
             </div>
-          </>
-        )}
-        {ressource?.item?.get_lien_ressource_commentaire &&
-          ressource.item.get_lien_ressource_commentaire.map((comment) => {
-            return (
-              <div className={styles.commentContainer} key={comment.id}>
-                <p className={styles.commentAuthor}>
-                  {comment.get_utilisateur.pseudonyme}
-                </p>
-                <p className={styles.commentAuthor}>{comment.commentaire}</p>
-              </div>
-            );
-          })}
-      </div>
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <h2>Commentaire créé avec succès</h2>
-        <p>En attente de validation...</p>
-      </Modal>
+            {ressource && (
+              <>
+                <img
+                  src={ressource.item.url_res}
+                  alt="Resource"
+                  className={styles.imageContainer}
+                />
+                <div className={styles.characteristicProp}>
+                  <p className={styles.category}>
+                    Catégorie :{" "}
+                    {ressource.item.get_lien_ressource_categorie
+                      .map(
+                        (lien: { get_categorie: { intitule_cat: string } }) =>
+                          lien.get_categorie.intitule_cat
+                      )
+                      .join(" / ")}
+                  </p>
+                  <p className={styles.typeOfRelation}>
+                    Types de relation :{" "}
+                    {ressource?.item.get_lien_ressource_relation
+                      .map(
+                        (lien: {
+                          get_relation_ressource: { intitule_rel: string };
+                        }) => lien.get_relation_ressource.intitule_rel
+                      )
+                      .join(" / ")}
+                  </p>
+                  <p className={styles.typeOfResource}>
+                    Type de ressource :{" "}
+                    {ressource.item.get_type_ressource.intitule_type_res}
+                  </p>
+                </div>
+                <div className={styles.content}>
+                  <h1 className={styles.title}>{ressource.item.titre_res}</h1>
+                  <p className={styles.resourceContent}>
+                    {ressource.item.contenu_res}
+                  </p>
+                </div>
+              </>
+            )}
+            <div className={styles.headerComment}>
+              <h3 className={styles.comment}>Commentaires</h3>
+              <button
+                className={styles.icon}
+                onClick={() => user && setComment(true)}
+              >
+                +
+              </button>
+            </div>
+            {comment && (
+              <>
+                <input
+                  ref={inputRef}
+                  type="text"
+                  placeholder="Vous pouvez écrire votre commentaire"
+                  value={commentText}
+                  onChange={(e) => setCommentText(e.target.value)}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      postComment(
+                        ressource.item.id,
+                        user.id,
+                        token,
+                        inputRef.current.value
+                      );
+                    }
+                  }}
+                ></input>
+                <div className={styles.publish}>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() =>
+                      postComment(
+                        ressource.item.id,
+                        user.id,
+                        token,
+                        commentText
+                      )
+                    }
+                  >
+                    Soumettre le commentaire
+                  </button>
+                </div>
+              </>
+            )}
+            {ressource?.item?.get_lien_ressource_commentaire &&
+              ressource.item.get_lien_ressource_commentaire.map((comment) => {
+                return (
+                  <div className={styles.commentContainer} key={comment.id}>
+                    <p className={styles.commentAuthor}>
+                      {comment.get_utilisateur.pseudonyme}
+                    </p>
+                    <p className={styles.commentAuthor}>
+                      {comment.commentaire}
+                    </p>
+                  </div>
+                );
+              })}
+          </div>
+          <Modal isOpen={isModalOpen} onClose={closeModal}>
+            <h2>Commentaire créé avec succès</h2>
+            <p>En attente de validation...</p>
+          </Modal>
+        </>
+      )}
     </div>
   );
 };
